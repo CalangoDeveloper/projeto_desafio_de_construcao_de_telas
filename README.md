4eed507c2414cd4419d41cef57247ed8bedc3b9a
CalangoDeveloper <antonio.lustosa1@aluno.ce.gov.br>
2024-04-29 19:43:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto_desafio_de_construcao_de_telas
+ 
---
+++ android/app/src/main/kotlin/com/example/projeto_desafio_de_construcao_de_telas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.projeto_desafio_de_construcao_de_telas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
